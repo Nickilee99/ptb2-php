285ca2d2ba07d0fb676ce258b0c60eb9851de4a9
Nickilee99 <43326219+Nickilee99@users.noreply.github.com>
2020-04-23 09:15:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
   <script>
-    function getLinkJSONpt2() {
+    function getLinkJSONtamgiac() {
         var a,b,c;
         a = document.getElementById("inputa").value;
         b = document.getElementById("inputb").value;
         c = document.getElementById("inputc").value;
-        window.open("api.php/checkptbac2/"+a+"/"+b+"/"+c);
+        window.open("api.php/checktamgiac/"+a+"/"+b+"/"+c);
     }
-    function getLinkJSONyear() {
+    function getLinkJSONngayketiep() {
         var a;
-        a = document.getElementById("inputyear").value;
-        window.open("api.php/checkyear/"+a);
+        a = document.getElementById("inputday").value;
+        b = document.getElementById("inputmonth").value;
+        c = document.getElementById("inputyear").value;
+        window.open("api.php/checkngayketiep/"+a+"/"+b+"/"+c);
     }
     function getData()
     {
-        document.getElementById("txt_apijsonpt2").innerHTML = window.location.host +"/CloudPhpEX/api.php/checkptbac2/a/b/c";
-        document.getElementById("txt_apijsonyear").innerHTML = window.location.host +"/CloudPhpEX/api.php/checkyear/year";
+        document.getElementById("txt_apijsonpt2").innerHTML = window.location.host +"/CloudPhpEX/api.php/checktamgiac/a/b/c";
+        document.getElementById("txt_apijsonyear").innerHTML = window.location.host +"/CloudPhpEX/api.php/checkngayketiep/day/month/year";
     }
 </script>
 
@@ -27,8 +29,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Giải phương trình bậc 2</h1>
-<a>ax² + bx + c = 0</a>
+<h1>Kiem tra tam giac</h1>
+<a>a,b,c</a>
 <form>
     <label for="fname">a</label><br>
     <input type="number" id="inputa" name="fname"><br>
@@ -39,9 +41,13 @@
     <input type ="button" name="OKE" value="GETJSON" onclick="getLinkJSONpt2()">
 </form>
 <h3>Api json pt2: </h3><h4 id="txt_apijsonpt2"></h4>
-<h1>Kiểm tra năm nhuần</h1>
+<h1>Kiểm tra ngày kế tiếp</h1>
 <form>
-    <label for="lname">Nhập năm</label><br>
+    <label for="lname">Nhập ngày</label><br>
+    <input type="number" id="inputday" name="lname"><br>
+   <label for="lname">Nhập tháng</label><br>
+    <input type="number" id="inputmonth" name="lname"><br>
+   <label for="lname">Nhập năm</label><br>
     <input type="number" id="inputyear" name="lname"><br>
     <input type ="button" name="OKE" value="GETJSON" onclick="getLinkJSONyear()">
 </form>
